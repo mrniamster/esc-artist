36ae605d07ddee56c8ce4ea516f64e03f0bfe57c
mrniamster <you@example.com>
2024-04-02 10:28:51 +0530

    fixplay

--- src/pages/LandingPage.vue
+++ src/pages/LandingPage.vue
@@ -4,20 +4,30 @@
     <p>Help the fearless cops capture the fugitive hiding in one of the neighboring cities.</p>
     <img  src="https://i.ibb.co/jHkLC2M/image.png" class="img-fluid"/>
     <p>
-      <router-link to="/city-selection">
-      <button @click="play">Start Game</button>
-    </router-link>
+        <button @click="play">Start Game</button>
+      
     </p>
   </div>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
+import { useRouter } from 'vue-router';
 export default {
-  name: 'LandingPage'
-}
+  name: 'LandingPage',
+  setup() {
+    const router = useRouter()
 const play = () => {
    var audio = document.getElementById("myAudio");
   audio.play();
+  console.log('playing audio')
+  router.push('/city-selection');
 }
+    return {
+      play
+    }
+  }
+}
+
+ 
 </script>
